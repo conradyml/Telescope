340d82baf0c66d9c12aae30c394546bdb393dec3
conradyml <conradyml@gmail.com>
2025-02-15 21:18:40 -0500

    azimuth work in progress

--- telescope.py
+++ telescope.py
@@ -13,6 +13,10 @@ from flask_restful import Resource, Api
 
 kit = MotorKit(address=0x60)
 
+for i in range(200):
+	kit.stepper1.onestep()
+kit.stepper1.release()
+
 # creating the flask app 
 app = Flask(__name__) 
 # creating an API object 
@@ -35,9 +39,9 @@ class Hello(Resource):
 		data = request.get_json()	 # status code 
 		return jsonify({'data': data}), 201
 
-class stepperMotor(Resource):
-	def __init__(self, path):
-		Resource.__init__(self, path)
+class azimuth(Resource):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
 		#self.stepper_motor = stepm
 		self.stepper_motor = kit.stepper1
 		#self.steps_per_degree=steps
@@ -50,7 +54,7 @@ class stepperMotor(Resource):
 
 	def put(self):
 
-		data = request.get_jason()
+		data = request.get_json()
 
 		if 'angle' not in data:
 			raise InvalidAPIUsage("angle data is missing from payload!", status_code=404)
@@ -60,10 +64,10 @@ class stepperMotor(Resource):
 		if newAngle < self.currentAngle:
 			direction = stepper.BACKWARD
 
-		changeSteps = abs(currentAngle-newAngle)*self.steps_per_degree
+		changeSteps = abs(self.currentAngle-newAngle)*self.steps_per_degree
 
 		for i in range(changeSteps):
-			self.stepper_motor(direction=direction, style=stepper.INTERLEAVE)
+			self.stepper_motor.onestep(direction=direction, style=stepper.INTERLEAVE)
 
 		self.currentAngle = newAngle
 		return jsonify({'angle': self.currentAngle})
@@ -96,9 +100,9 @@ class InvalidAPIUsage(Exception):
 # adding the defined resources along with their corresponding urls 
 api.add_resource(Hello, '/') 
 api.add_resource(Square, '/square/<int:num>') 
-api.add_resource(stepperMotor, '/azimuth')
+api.add_resource(azimuth, '/azimuth/')
 
-@api.errorhandler(InvalidAPIUsage)
+@app.errorhandler(InvalidAPIUsage)
 def invalid_api_usage(e):
 	return jsonify(e.to_dict()), e.status_code
 
@@ -106,4 +110,4 @@ def invalid_api_usage(e):
 # driver function 
 if __name__ == '__main__': 
 
-	app.run(debug = True) 
+	app.run(debug = True, host="0.0.0.0", port=5000) 
