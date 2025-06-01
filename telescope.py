94d9a9e901cadfeb0b1cc7b3b747919604efcf7b
conradyml <conradyml@gmail.com>
2025-05-31 20:16:30 -0400

    remove MotorKit references
    MotorKit was limiting use of servos.

--- telescope.py
+++ telescope.py
@@ -3,18 +3,27 @@ import board
 from busio import I2C
 
 # using adafruit motor hat
-#from adafruit_pca9685 import PCA9685
-#from adafruit_motor import servo
-from adafruit_motorkit import MotorKit
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
+#from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 
 i2c = I2C(board.SCL, board.SDA, frequency=400_000)
 
-kit = MotorKit(i2c=i2c,address=0x60)
+#kit = MotorKit(i2c=i2c,address=0x60)
 #kit.frequency(50)
-#pca = PCA9685(board.I2C(),address=0x60)
-#pca.frequency = 50
-#servo1 = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2400,actuation_range=135)
+pca = PCA9685(board.I2C(),address=0x60)
+pca.frequency = 1600
+servo1 = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2400,actuation_range=135)
+# setting stepper1 to use M1 and M2 on the hat.
+pca.channels[8].duty_cycle = 0xFFFF
+pca.channels[13].duty_cycle = 0xFFFF
+stepper1 = stepper.Motor(pca.channels[9], pca.channels[10], pca.channels[11], pca.channels[12])
+
+# setting stepper2 to use M3 and M4 on the hat.
+pca.channels[7].duty_cycle = 0xFFFF
+pca.channels[2].duty_cycle = 0xFFFF
+stepper2 = stepper.Motor(pca.channels[4], pca.channels[3], pca.channels[5], pca.channels[6])
 
 #In astronomy, "telescope azimuth" refers to the horizontal angle of a telescope's pointing direction, measured 
 # clockwise from north, while "elevation" refers to the vertical angle of the telescope pointing upwards from the horizon, 
@@ -44,11 +53,11 @@ class Telescope:
 	def __init__(self,azimuthDegree,elevationDegree):
 		self.position = Position(0,0)
 		self.focus = 0
-		self.azimuth_motor = kit.stepper2
+		self.azimuth_motor = stepper2
 		self.azimuth_steps_per_degree = azimuthDegree
-		self.elevation_motor = kit.stepper1
+		self.elevation_motor = stepper1
 		self.elevation_steps_per_degree = elevationDegree
-#		self.focus_motor = servo1
+		self.focus_motor = servo1
 
 	def set_azimuth(self,newAzimuth):
 
