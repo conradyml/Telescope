0213eccfec3947a4aeca0de5bf0d46d427091d0e
conradyml <conradyml@gmail.com>
2025-08-04 20:15:01 -0400

    Fix threading

--- telescope.py
+++ telescope.py
@@ -44,7 +44,7 @@ class Telescope:
 	def set_azimuth(self,newAzimuth):
 
 		changeSteps = int((self.position.azimuth-newAzimuth)*self.azimuth_steps_per_degree)
-		self.threadA = threading.Thread(target=self.azimuth_motor.move, args=(changeSteps))
+		self.threadA = threading.Thread(target=self.azimuth_motor.move, args=(changeSteps,))
 		self.threadA.start()
 		#self.azimuth_motor.move(changeSteps)
 		self.azimuth_motor.sleep()
